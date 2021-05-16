c535ebeac59a6d0630beeeef6e11d613e33cb4e6
Kibet 1998 <ronoronald1998@gmail.com>
2021-05-16 13:58:02 +0300

    add initial files

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from "../quote";
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+  quotes:Quote[] = [
+    new Quote(1, 'Ronnie', 'Kennedy','Change the world by being yourself'),
+    new Quote(1, 'Ronnie', 'Hillary', 'Every moment is a fresh beginning'),
+    new Quote(1, 'Ronnie', 'Gilbert', 'Never regret anything that once made you happy'),
+    new Quote(1, 'Ronnie', 'Mercy','Die with memories not dreams'),
+    new Quote(1, 'Ronnie', 'Nickman', 'Aspire to inspire before we expire'),
+   
+    
+  ];
+  toggleDetails(index){
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+  completeQuote(isComplete, index){
+    if (isComplete) {
+      this.quotes.splice(index,1);
+    }
+  }
+
+
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
